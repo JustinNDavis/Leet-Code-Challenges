7df0e6cbf60762d98f9a62c0dbd0069ee84b5c90
JustinNDavis <59874785+JustinNDavis@users.noreply.github.com>
2020-02-14 12:32:39 -0500

    added challenge code
    Faster than 99.30% of other submissions.
    Less memory than 100% of other submissions.

---
+++ Challenge1281.cs
@@ -0,0 +1,25 @@
+﻿//Author: Justin Davis
+//Date: 2/14/2020
+//Purpose: The purpose to complete leet code challenge
+//Faster than 99.30% of other solutions
+//Less memory used than 100% of other solutions 
+public class Solution
+{
+    public int SubtractProductAndSum(int n)
+    {
+        int temp;
+        string temp2;
+        int product = 1;
+        int sum = 0;
+        string nn = n.ToString();
+        for (int i = 0; i < nn.Length; i++)
+        {
+            temp2 = (nn.Substring(i, 1));
+            temp = int.Parse(temp2);
+            product *= temp;
+            sum += temp;
+        }
+        int result = product - sum;
+        return result;
+    }
+}
